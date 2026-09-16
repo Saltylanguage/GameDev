e474b0095075576a95eafca350dc7a1bf93a8819
Simmasterbev <Nicholas.Nevins@queuecraft-dev.com>
2026-09-16 02:16:30 -0400

    Fix restart actions starting expeditions

--- LearningIndieDev/Assets/Scripts/Game/Presentation/SpeciesSimulationPreview.cs
+++ LearningIndieDev/Assets/Scripts/Game/Presentation/SpeciesSimulationPreview.cs
@@ -1637,6 +1637,7 @@ namespace SaltyGame
             if (previewState == SpeciesPreviewState.Results)
             {
                 PrepareNextRun();
+                StartSimulation();
             }
         }
 
